5a0961d1932c87befe6f20d35f9660e885531089
Qunthunnan <bredtv6@gmail.com>
2024-05-24 22:53:07 +0300

    learning style components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
  import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import styled from 'styled-components';
 
 function App() {
   return (
@@ -31,9 +32,26 @@ function FooButton() {
 //Компоненти можуть бути як класовими 
 
 class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: 0
+    }
+  }
   render () {
-    let num = 0;
-    const btn = <button className='Button'>Натисків: {num}</button>;
+    const {num} = this.state;
+    const Btn = styled.button`
+      padding: 100px;
+      font-size: 30px;
+      font-weight: 700;
+      background-color: burlywood;
+      border: 2px darkcyan dashed;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    `;
+    const btn = <Btn onClick={()=>{this.setState(({num}) => { return {num: num + 1} })}} className='Button'>Натисків: {num}</Btn>;
     return btn;
   }
 }
